f820a96e4bf8c3465c13111d640e7baf5fe2d223
karenconley <karen.c.echeverria@gmail.com>
2018-01-10 09:28:06 -0300

    maqueta index con slider ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,5 +3,19 @@ $( document ).ready(function(){
   $('.carousel').carousel();
   $('.modal').modal();
   $('ul.tabs').tabs();
-
 })
+
+
+
+//slider nuevo
+$(document).ready(function(){
+  $('.carousel').carousel({
+  duration: 500,
+  shift: 40
+  });
+
+$('.next').click(function(){
+ })
+ });
+
+$('.carousel.carousel-slider').carousel({fullWidth: true});
