024ef0f315d781b1514ce5d9170f36bd5ee64425
conois <conii.echeverria@gmail.com>
2018-01-11 20:46:19 -0300

    review de vuelta

--- assets/js/all.js
+++ assets/js/all.js
@@ -3,15 +3,15 @@
 $(document).ready(() => {
 	console.log("cargado")
 	printMovie();
-	imprimirReview();
-}); 
+
+}) //document.ready 
 
 //Funcion que imprime las series en la pagina all.html 
 function printMovie(){
 	console.log("se imprime pelicula")
 	$("#movie-box").show(); 
 	$("#serie-box").hide()
-	for ( var i in peliculas){
+	for (var i in peliculas){
 		var titleMovie= peliculas[i].title; 
 		var trailerMovie= peliculas[i].trailer;
 		axios.get('http://www.omdbapi.com?t='+ titleMovie + '&apikey=a7a10611')
@@ -32,6 +32,8 @@ function printMovie(){
 			let movieDirector= response.data.Director;
 			let movieRating= response.data.Ratings[0].Value; 
 			let movieReleased= response.data.Released
+			let movieId= response.data.imdbID
+
 
 			$("#movie-box").append('<div class="row box">'+
 												      '<div class="col s12 m5">'+
@@ -49,7 +51,7 @@ function printMovie(){
 												        '<br>'+
 												        '<div>'+
 												          '<a class="waves-effect waves-light btn red darken-4 trailer"><i class="fa fa-caret-right" aria-hidden="true"></i></i>  Trailer</a>'+
-												          '<a class="waves-effect waves-light btn blue darken-4" href="review.html"><i class="fa fa-commenting" aria-hidden="true"></i>  Teorías</a>'+
+												          '<a id="'+ movieId +'" class="waves-effect waves-light btn blue darken-4  btn-movie-teoria" href="review.html"><i class="fa fa-commenting" aria-hidden="true"></i>  Teorías</a>'+
 												        '</div>'+
 												      '</div>'+
 												    '</div><!--boxpelis-->'+
@@ -59,7 +61,7 @@ function printMovie(){
 			console.log("error"); 
 		})
 	} 
-}
+};
 
 function printSeries(){
 	console.log("se imprime serie")
@@ -101,7 +103,7 @@ function printSeries(){
 												        '<br>'+
 												        '<div>'+
 												          '<a class="waves-effect waves-light btn red darken-4 btn-trailer"><i class="fa fa-caret-right" aria-hidden="true"></i></i>  Trailer</a>'+
-												          '<a class="waves-effect waves-light btn blue darken-4 btn-teorias" href="review.html" onclick=><i class="fa fa-commenting" aria-hidden="true"></i>  Teorías</a>'+
+												          '<a class="waves-effect waves-light btn blue darken-4 btn-teorias" href="review.html"><i class="fa fa-commenting" aria-hidden="true"></i>  Teorías</a>'+
 												        '</div>'+
 												      '</div>'+
 												    '</div><!--boxpelis-->'+
@@ -115,6 +117,32 @@ function printSeries(){
 		})
 	};
 }; 
+
+
+/* for (var j in peliculas){
+			if (idbtn === peliculas[j].idTeoria){
+				var titlePelicula=peliculas[j].title
+				axios.get('http://www.omdbapi.com?t='+ titlePelicula + '&apikey=a7a10611')
+				.then((response) => {
+						let movieTitle= response.data.Title; 
+						let movieActors= response.data.Actors;
+						let movieAward= response.data.Awards; 
+						let movieDescription= response.data.Plot; 
+						let movieYear= response.data.Year;
+						let movieImdbRating = response.data.imdbRating; 
+						let movieRuntime= response.data.Runtime; 
+						let moviePoster= response.data.Poster; 
+						let movieDirector= response.data.Director;
+						let movieRating= response.data.Ratings[0].Value; 
+
+				console.log("sisi")
+				$("#box-review").append('<p> hola </p>')
+
+				})
+			}
+		}*/ 
+
+/*
 
  function imprimirReview(){
 	console.log("se imprime review"); 
@@ -140,7 +168,7 @@ function printSeries(){
 			let movieRating= response.data.Ratings[0].Value; 
 			let movieReleased= response.data.Released; 
 
-			$("#box-review").append('<div class="row">'+
+			$("#box-review").append('<div class="row esconder">'+
 													      '<div class="col s12">'+
 													        '<img id="" class="imgMovie" src=' + moviePoster +' alt="caratula_pelicula">'+
 													        '<h2 class="title center-align">'+ movieTitle +'</h2>'+
@@ -150,12 +178,13 @@ function printSeries(){
 													        '<!-- sinopsis -->'+
 													        '<p class="description center-align flow-text">'+ movieDescription +'</p>'+
 													      '</div> <!-- /col s12 -->'+
-													    '</div> <!-- /row -->'); 
+													    '</div> <!-- /row -->')
 
-			})
+		})
 		.catch((error) => {
 			console.log("error"); 
-	
 		})
-	} 
+	}
 };
+
+*/
--- assets/js/data/peliculas.js
+++ assets/js/data/peliculas.js
@@ -1,49 +1,64 @@
 var peliculas = [
 {	"title": "inception", 
-	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU"
+	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU",
+	"idTeoria":"tt1375666"
 }, 
 {	"title": "black+swan", 
-	"trailer": "https://www.youtube.com/embed/ujT_3QZA0ZE"
+	"trailer": "https://www.youtube.com/embed/ujT_3QZA0ZE",
+	"idTeoria":"tt0947798"
 }, 
 {	"title": "the+shining", 
-	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU"
+	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU",
+	"idTeoria":"tt0081505"
 },
 {	"title": "memento", 
-	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU"
-},
-{	"title": "eternal+sunshine+of+the+spotless+mind", 
-	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU"
-},
-{	"title": "fight+club", 
-	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU"
-}, 
-{	"title": "vertigo", 
-	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU"
+	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU",
+	"idTeoria":"tt0209144"
 
 },
+{	"title": "eternal+sunshine+of+the+spotless+mind", 
+	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU",
+	"idTeoria":"tt0338013"
+},
+{	"title": "fight+club", 
+	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU",
+	"idTeoria":"tt0137523"
+}, 
+{	"title": "vertigo", 
+	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU",
+	"idTeoria":"tt0052357"
+},
 {	"title": "se7en", 
-	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU"
+	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU",
+	"idTeoria":"tt0114369"
 },
 {	"title": "the+machinist", 
-	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU"
+	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU",
+	"idTeoria":"tt0361862"
 },
 {	"title": "the+sixth+sense", 
-	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU"
+	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU",
+	"idTeoria":"tt0167404"
 },
 {	"title": "panic+room", 
-	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU"
+	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU",
+	"idTeoria":"tt0258000"
 },
 {	"title": "gone+girl", 
-	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU" 
+	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU",
+	"idTeoria":"tt2267998"
 }, 
 {	"title": "manhunter", 
-	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU"
+	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU",
+	"idTeoria":"tt0091474"
 },
 {	"title": "blue+velvet", 
-	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU"
+	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU",
+	"idTeoria":"tt1130884"
 },
 { "title": "shutter+island", 
-	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU" 
+	"trailer": "https://www.youtube.com/embed/OCEkhKvm-hU",
+	"idTeoria":"tt0090756"
 }
 
 
